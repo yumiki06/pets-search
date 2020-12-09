fb8a678d4f4d86d81255343719575162aa3e67cb
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-12-10 00:28:57 +0900

    Add create action & params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,14 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:name, :text, :image)
+  end
+
 end
