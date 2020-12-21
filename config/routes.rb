233e7392c7f583db17cad3aff89c10ca953adf2c
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-12-21 23:45:11 +0900

    Add edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new, :create, :destroy]
   root to: 'posts#index'
+  resources :posts, only: [:index, :new, :create, :destroy, :edit]
 end
